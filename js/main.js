d9603625a170e66e7901b2f21633e15d519046a1
AlessandroPedroso <alessandropedrosoti@gmail.com>
2023-02-01 06:18:53 -0300

    projeto concluído

--- js/main.js
+++ js/main.js
@@ -1,4 +1,51 @@
 const controlador = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+
+
+controlador.forEach(item =>{
+
+    item.addEventListener("click", (evento)=>{
+
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode.querySelector("[data-contador]"))
+        atualizaEstatisticas(evento.target.dataset.peca)
+
+    })
+})
 
 
 const manipulaDados = (operacao, controle) =>{
@@ -15,14 +62,14 @@ const manipulaDados = (operacao, controle) =>{
     }
  }
 
-controlador.forEach(item =>{
+ const atualizaEstatisticas = (peca) =>{
+    console.log(pecas[peca])
 
-    item.addEventListener("click", (evento)=>{
-
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode.querySelector("[data-contador]"))
-
+    estatisticas.forEach((elemento)=>{
+        
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
     })
-})
+ }
 
 
  
